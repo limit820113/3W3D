0517f1a680b3123a7bba48122c4634b3003f67a3
limit820113 <limit820113@yahoo.com.tw>
2015-07-15 23:46:26 +0800

    L

---
+++ WebGL/triangle2_files/triangle2.js
@@ -0,0 +1,70 @@
+"use strict";
+
+var gl;
+var points;
+var initVS = [-1, -1, 0, 1, 1, -1];
+var vertices = [];
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+	
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+	
+	smaller(initVS, 5);
+
+    //  Configure WebGL
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    //  Load shaders and initialize attribute buffers
+
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    gl.uniform1f(
+		gl.getUniformLocation ( program, "angle" ), 
+		1.0
+	);
+
+    // Load the data into the GPU
+
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    render();
+};
+
+function smaller(vs, level) {
+	if (level == 1) {
+		for (var i = 0; i < vs.length; i++)
+			vertices.push(vs[i]);
+	}
+	else {
+		for (var i = 0; i < 3; i++) {
+			var t = [];
+			
+			for (var j = 0; j < 3; j++)
+				t.push((vs[i*2]+vs[(i+j)%3*2])/2, (vs[i*2+1]+vs[(i+j)%3*2+1])/2);
+			
+			smaller(t, level-1);
+		}
+	}
+}
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLES, 0, vertices.length/2 );
+}
